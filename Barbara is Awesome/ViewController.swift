10f0ae8dd68d02f57c5f662991caf74ef32dea15
Barbara Ann Cleary <barbaracleary@barbaras-macbook-pro.local>
2018-01-22 09:20:15 -0500

    Initial Commit

---
+++ Barbara is Awesome/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Barbara is Awesome
+//
+//  Created by Barbara Ann Cleary on 1/22/18.
+//  Copyright © 2018 Barbara Cleary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
